020309acae196591c74a066400b26bd710b3fadf
곽현민 <hunmin111@gmail.com>
2026-01-07 18:59:21 +0900

    feat: add 3 layer architecture class

---
+++ src/main/java/com/scheduleappdevelop/schedule/service/ScheduleService.java
@@ -0,0 +1,11 @@
+package com.scheduleappdevelop.schedule.service;
+
+import com.scheduleappdevelop.schedule.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+}
