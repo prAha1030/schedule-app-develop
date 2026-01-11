f91f3cab3c1a2d94885aca3d4bb2f605ab5967fd
곽현민 <hunmin111@gmail.com>
2026-01-11 21:31:29 +0900

    feat: check comments in a schedule

--- src/main/java/com/scheduleappdevelop/comment/repository/CommentRepository.java
+++ src/main/java/com/scheduleappdevelop/comment/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.scheduleappdevelop.comment.repository;
 
 import com.scheduleappdevelop.comment.entity.Comment;
+import com.scheduleappdevelop.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+    List<Comment> findBySchedule(Schedule schedule);
 }
