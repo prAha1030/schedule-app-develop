8cddf3b55d28ba0464a364e1c62cd01b9e066657
곽현민 <hunmin111@gmail.com>
2026-01-11 23:01:46 +0900

    feat: get schedule pages

--- src/main/java/com/scheduleappdevelop/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/scheduleappdevelop/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,10 @@
 package com.scheduleappdevelop.schedule.repository;
 
 import com.scheduleappdevelop.schedule.entity.Schedule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    Page<Schedule> findAllBy(Pageable pageable);
 }
